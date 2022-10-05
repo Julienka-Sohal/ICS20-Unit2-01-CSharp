dac78c34c2447bc43d8427dab831ae49a3691be3
Julienka-Sohal <113055709+Julienka-Sohal@users.noreply.github.com>
2022-10-05 18:23:34 +0000

    program

--- main.cs
+++ main.cs
@@ -1,13 +1,15 @@
-// Created by: Mr Coxall
+// Created by: Julienka Sohal
 // Created on: Oct 2020
 //
 // This program displays, "Hello, World!"
+
 using System;
 
-class Program {
-  public static void Main (string[] args) 
-  {
-    Console.WriteLine ("Hello World");
-    Console.WriteLine ("\nDone.");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("\nDone.");
+    }
 }
